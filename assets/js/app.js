6cf5cdb23de4b50b093406f0f63f027a9f622afe
manucarenjot <carenjotje@gmail.com>
2021-12-10 16:50:16 +0100

    projet

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,29 @@
-let body = document.querySelector("body");
+
 let buttonOpen = document.getElementById('fenetre');
-let div = document.createElement("div")
+let div = document.createElement("div");
+let background = document.getElementById('background');
 
 
 function open(){
-    let features = 'menubar=no, scrollbars=yes, width=500, height=500, statut=no, toolbar=no'
-    div.open(' ', '', features);
-    div.style.width = '500px';
-    div.style.height = '500px';
-    div.style.border = 'solid 5px black';
-    div.style.backgroundColor = 'red'
-    body.append(div);
+    buttonOpen.addEventListener("click", ()=> {
+        innerHeight = window.innerHeight;
+        innerWidth = window.innerWidth;
+        background.append(div);
+        background.style.backgroundColor = 'rgba(0,0,0,0.21)';
+        background.style.width = '100%';
+        background.style.height = 'max-height';
+        div.style.width = '45%';
+        div.style.height = '500px';
+        div.style.display = 'flex';
+        div.style.marginLeft = '25%';
+        div.style.marginTop = '10rem';
+        div.style.border = "1rem solid black";
+        div.style.fontSize = "2rem";
+    })
+
 }
 
-buttonOpen.addEventListener("click", ()=> {
-    open();
-})
+open()
+/**
+let features = 'menubar=no, scrollbars=yes, width=500, height=500, statut=no, toolbar=no';
+ */
