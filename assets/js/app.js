ae7792eb53a157f3e5ce1c532c91bb4f34cc7c22
manucarenjot <carenjotje@gmail.com>
2021-12-12 12:52:51 +0100

    fenetre modale

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-
+let body = document.querySelector("body");
 let buttonOpen = document.getElementById('fenetre');
 let div = document.createElement("div");
 let background = document.getElementById('background');
@@ -6,18 +6,47 @@ let background = document.getElementById('background');
 
 function open(){
     buttonOpen.addEventListener("click", ()=> {
-        innerHeight = window.innerHeight;
-        innerWidth = window.innerWidth;
         background.append(div);
-        background.style.backgroundColor = 'rgba(0,0,0,0.21)';
-        background.style.width = '100%';
-        background.style.height = 'max-height';
-        div.style.width = '45%';
-        div.style.height = '500px';
+        background.style.display = 'flex';
+        background.style.flexDirection = 'column';
+        background.style.alignItems = 'center';
+        body.style.backgroundColor = 'rgb(100,114,106)';
+        div.style.width = '40%';
+        div.style.height = '450px';
         div.style.display = 'flex';
-        div.style.marginLeft = '25%';
-        div.style.marginTop = '10rem';
-        div.style.border = "1rem solid black";
+        div.style.border = "10px ridge coral";
+        div.style.backgroundColor = 'white';
+        div.style.borderRadius = '20px';
+        div.style.marginTop = '10%';
+        buttonOpen.remove();
+
+        div.style.display = 'flex';
+        div.style.flexDirection = 'column';
+        div.style.alignItems = 'center';
+        div.style.alignContent = 'space-around';
+        div.style.justifyContent = 'center';
+
+        let input1 = document.createElement('input');
+        let input2 = document.createElement('input');
+        let connection = document.createElement('button');
+
+        div.appendChild(input1);
+        div.appendChild(input2);
+        div.appendChild(connection);
+
+        input1.placeholder = "Votre E-mail";
+        input2.placeholder = "Votre mot de passe";
+        input2.type = "password";
+
+        input1.style.width = '40%';
+        input1.style.height = '40px';
+        input1.style.margin = '15px';
+        input2.style.width = '40%';
+        input2.style.height = '40px';
+        input2.style.margin = '15px';
+        connection.style.width = '30%';
+        connection.style.height = '30px';
+        connection.innerHTML = 'Connection'
         div.style.fontSize = "2rem";
     })
 
@@ -26,4 +55,7 @@ function open(){
 open()
 /**
 let features = 'menubar=no, scrollbars=yes, width=500, height=500, statut=no, toolbar=no';
+
+ div.style.marginLeft = '25%';
+
  */
